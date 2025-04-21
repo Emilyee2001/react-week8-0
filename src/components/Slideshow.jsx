327c1de5a8a5e5bbac46e4567be96d703c1a7a81
emilyt <emilyz_2001@hotmail.com>
2025-04-21 09:34:09 +0100

    修正ESLint錯誤

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
 export default function Slideshow({ images }) {
 
@@ -10,10 +11,14 @@ export default function Slideshow({ images }) {
       setIndex((prevIndex) => (prevIndex + 1) % renderImages.length);
     }, 1100);
     return () => clearInterval(timer);
-  }, [images.toString()]);
+  }, [images, renderImages.length]);
 
 
   return (<>
     <img src={renderImages[index]} alt={`slideshow-image-${index + 1}`} className="object-cover" />
   </>)
 }
+
+Slideshow.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
